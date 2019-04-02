379a52f8f9433ff79d81a18089368bd4c11ec641
Bgoon <isg1153@naver.com>
2019-04-02 22:20:02 +0900

    -프로젝트명 변경 -프리뷰 구현

---
+++ PendulumMotionEditor/PendulumMotionEditor/Script/MotionGraph/MGData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepMotionEditor
+{
+	public class MGData
+	{
+		private const int MinHandleCount = 2;
+
+		private List<MGPoint> handleList;
+
+		public MGData() {
+			handleList = new List<MGPoint>();
+
+			for (int i = 0; i < MinHandleCount; ++i) {
+				handleList.Add(new MGPoint());
+			}
+		}
+		public float GetMotionTime(float time) {
+			return time;
+		}
+	}
+}
