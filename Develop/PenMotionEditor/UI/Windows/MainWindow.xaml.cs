9e40bfc2a54d3547680d0690f63d1280d882ed82
TTOOWA <isg1153@naver.com>
2023-02-20 23:50:23 +0900

    - LoopEngine을 외부에서 받아올 수 있도록 수정

--- Develop/PenMotionEditor/UI/Windows/MainWindow.xaml.cs
+++ Develop/PenMotionEditor/UI/Windows/MainWindow.xaml.cs
@@ -3,80 +3,89 @@ using PenMotionEditor.UI.FX;
 using System.ComponentModel;
 using System.Windows;
 
-namespace PenMotionEditor.UI.Windows {
-	/// <summary>
-	/// MainWindow.xaml에 대한 상호 작용 논리
-	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow() {
-			InitializeComponent();
-			if (this.IsDesignMode())
-				return;
+namespace PenMotionEditor.UI.Windows; 
 
-			Init();
-			RegisterEvents();
-		}
+/// <summary>
+///     MainWindow.xaml에 대한 상호 작용 논리
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        if (this.IsDesignMode()) {
+            return;
+        }
 
-		private void Init() {
-			Cursor = EditorContext.CursorStorage.cursor_default;
-			SetContentContextVisible(false);
-		}
-		private void RegisterEvents() {
-			Closing += OnClosing;
+        Init();
+        AddEventListeners();
+    }
 
-			EditorContext.LoopEngine.AddLoopAction(OnTick);
+    private void Init() {
+        Cursor = EditorContext.CursorStorage.cursor_default;
+        SetContentContextVisible(false);
 
-			//FileManagerBar
-			FileManagerBar.CreateFileButtonClick += NewFileButton_OnClick;
-			FileManagerBar.OpenFileButtonClick += OpenFileButton_OnClick;
-			FileManagerBar.SaveFileButtonClick += SaveFileButton_OnClick;
-		}
+        EditorContext.Init();
+    }
 
-		private void OnClosing(object sender, CancelEventArgs e) {
-			if (!EditorContext.IsSaved) {
-				if (!EditorContext.ShowSaveQuestion()) {
-					e.Cancel = true;
-				}
-			}
-		}
-		private void OnTick() {
-			if (MouseInput.Left.IsDown) {
-				Vector2 fxPos = MouseInput.AbsolutePosition - FxCanvas.GetAbsolutePosition();
+    private void AddEventListeners() {
+        Closing += OnClosing;
 
-				ClickFx fx = new ClickFx();
-				fx.SetParent(FxCanvas);
-				fx.SetCanvasPosition(fxPos);
-			}
-		}
-		private void NewFileButton_OnClick() {
-			if (EditorContext.CreateFile()) {
-				SetContentContextVisible(true);
-			}
-		}
-		private void OpenFileButton_OnClick() {
-			if (EditorContext.OpenFile()) {
-				SetContentContextVisible(true);
-			}
-		}
-		private void SaveFileButton_OnClick() {
-			EditorContext.SaveFile();
-		}
+        EditorContext.LoopEngine.AddLoopAction(OnTick);
 
-		public void CreateNewFile() {
-			EditorContext.CreateFile();
-		}
-		public bool OpenFile() {
-			EditorContext.OpenFile();
-			return true;
-		}
-		public void SaveFile() {
-			EditorContext.SaveFile();
-		}
+        //FileManagerBar
+        FileManagerBar.CreateFileButtonClick += NewFileButton_OnClick;
+        FileManagerBar.OpenFileButtonClick += OpenFileButton_OnClick;
+        FileManagerBar.SaveFileButtonClick += SaveFileButton_OnClick;
+    }
 
-		//UI
-		public void SetContentContextVisible(bool show) {
-			EditorContext.Visibility = show ? Visibility.Visible : Visibility.Hidden;
-		}
+    private void OnClosing(object sender, CancelEventArgs e) {
+        if (!EditorContext.IsSaved) {
+            if (!EditorContext.ShowSaveQuestion()) {
+                e.Cancel = true;
+            }
+        }
+    }
 
-	}
+    private void OnTick() {
+        if (MouseInput.Left.IsDown) {
+            Vector2 fxPos = MouseInput.AbsolutePosition - FxCanvas.GetAbsolutePosition();
+
+            ClickFx fx = new();
+            fx.SetParent(FxCanvas);
+            fx.SetCanvasPosition(fxPos);
+        }
+    }
+
+    private void NewFileButton_OnClick() {
+        if (EditorContext.CreateFile()) {
+            SetContentContextVisible(true);
+        }
+    }
+
+    private void OpenFileButton_OnClick() {
+        if (EditorContext.OpenFile()) {
+            SetContentContextVisible(true);
+        }
+    }
+
+    private void SaveFileButton_OnClick() {
+        EditorContext.SaveFile();
+    }
+
+    public void CreateNewFile() {
+        EditorContext.CreateFile();
+    }
+
+    public bool OpenFile() {
+        EditorContext.OpenFile();
+        return true;
+    }
+
+    public void SaveFile() {
+        EditorContext.SaveFile();
+    }
+
+    //UI
+    public void SetContentContextVisible(bool show) {
+        EditorContext.Visibility = show ? Visibility.Visible : Visibility.Hidden;
+    }
 }
