cfd10e49bd2cba133e0bae85576e96318b5101ce
TTOOWA <isg1153@naver.com>
2023-02-20 23:50:23 +0900

    - LoopEngine을 외부에서 받아올 수 있도록 수정

--- Develop/PenMotionEditor/UI/Tabs/MotionEditorContext.xaml.cs
+++ Develop/PenMotionEditor/UI/Tabs/MotionEditorContext.xaml.cs
@@ -6,189 +6,199 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace PenMotionEditor.UI.Tabs {
-	public partial class MotionEditorContext : UserControl {
-		public static MotionEditorContext Instance {
-			get; private set;
-		}
+namespace PenMotionEditor.UI.Tabs;
 
-		//Modules
-		public GLoopEngine LoopEngine {
-			get; private set;
-		}
-		public CursorStorage CursorStorage {
-			get; private set;
-		}
+public partial class MotionEditorContext : UserControl {
+    public static MotionEditorContext Instance { get; private set; }
 
-		//EditingFile
-		public bool IsSaved {
-			get; private set;
-		}
-		public bool OnEditing => EditingFile != null;
-		public MotionFile EditingFile {
-			get; private set;
-		}
+    //Modules
+    public GLoopEngine LoopEngine { get; private set; }
+    public CursorStorage CursorStorage { get; private set; }
 
-		public MotionEditorContext() {
-			Instance = this;
+    //EditingFile
+    public bool IsSaved { get; private set; }
+    public bool OnEditing => EditingFile != null;
+    public MotionFile EditingFile { get; private set; }
 
-			InitializeComponent();
+    public MotionEditorContext() {
+        Instance = this;
 
-			if (this.IsDesignMode())
-				return;
+        InitializeComponent();
 
-			InitMembers();
-			InitTabs();
-			RegisterEvents();
-		}
+        if (this.IsDesignMode()) {
+            return;
+        }
+    }
 
-		private void InitTabs() {
-			MotionTab.Init(this);
-			GraphEditorTab.Init(this);
-			PreviewTab.Init(this);
-		}
-		private void InitMembers() {
-			LoopEngine = new GLoopEngine();
-			CursorStorage = new CursorStorage();
+    public void Init(GLoopEngine loopEngine = null) {
+        if (loopEngine != null) {
+            LoopEngine = loopEngine;
+        } else {
+            LoopEngine = new GLoopEngine();
 
-			IsSaved = true;
+            LoopEngine.StartLoop();
+            LoopEngine.MaxOverlapFrame = 1;
+        }
 
-			LoopEngine.StartLoop();
-			LoopEngine.MaxOverlapFrame = 1;
-		}
-		private void RegisterEvents() {
+        InitMembers();
+        InitTabs();
+        RegisterEvents();
+    }
 
-			LoopEngine.AddLoopAction(OnTick);
-		}
+    private void InitTabs() {
+        MotionTab.Init(this);
+        GraphEditorTab.Init(this);
+        PreviewTab.Init(this);
+    }
 
-		public bool CreateFile(bool checkSaved = true, bool createRootFolder = true) {
-			if (checkSaved && !ShowSaveQuestion())
-				return false;
+    private void InitMembers() {
+        CursorStorage = new CursorStorage();
 
-			CloseFile(false);
-			EditingFile = new MotionFile(createRootFolder);
+        IsSaved = true;
+    }
 
-			RegisterFileEvents(EditingFile);
+    private void RegisterEvents() {
+        LoopEngine.AddLoopAction(OnTick);
+    }
 
-			MarkSaved();
-			return true;
-		}
-		public bool OpenFile(bool checkSaved = true) {
-			if (checkSaved && !ShowSaveQuestion())
-				return false;
+    public bool CreateFile(bool checkSaved = true, bool createRootFolder = true) {
+        if (checkSaved && !ShowSaveQuestion()) {
+            return false;
+        }
 
-			string filePath = GetOpenFilename();
-			if (string.IsNullOrEmpty(filePath))
-				return false;
+        CloseFile(false);
+        EditingFile = new MotionFile(createRootFolder);
 
-			CloseFile(false);
-			EditingFile = new MotionFile(false);
+        RegisterFileEvents(EditingFile);
 
-			RegisterFileEvents(EditingFile);
+        MarkSaved();
+        return true;
+    }
 
-			EditingFile.Load(filePath);
+    public bool OpenFile(bool checkSaved = true) {
+        if (checkSaved && !ShowSaveQuestion()) {
+            return false;
+        }
 
-			MotionTab.UpdateItemPreviews();
+        string filePath = GetOpenFilename();
+        if (string.IsNullOrEmpty(filePath)) {
+            return false;
+        }
 
-			return true;
-		}
-		public bool SaveFile() {
-			string filePath = GetSaveFilename();
-			if (string.IsNullOrEmpty(filePath))
-				return false;
+        CloseFile(false);
+        EditingFile = new MotionFile(false);
 
-			EditingFile.Save(filePath);
+        RegisterFileEvents(EditingFile);
 
-			MarkSaved();
-			return true;
-		}
-		public bool CloseFile(bool checkSaved = true) {
-			if (!OnEditing)
-				return true;
-			if (checkSaved && !ShowSaveQuestion())
-				return false;
+        EditingFile.Load(filePath);
 
-			UnregisterFileEvents(EditingFile);
+        MotionTab.UpdateItemPreviews();
 
-			MotionTab.ClearItems();
-			GraphEditorTab.DetachMotion();
-			EditingFile = null;
+        return true;
+    }
 
-			return true;
-		}
+    public bool SaveFile() {
+        string filePath = GetSaveFilename();
+        if (string.IsNullOrEmpty(filePath)) {
+            return false;
+        }
 
-		public void MarkSaved() {
-			IsSaved = true;
-		}
-		public void MarkUnsaved() {
-			IsSaved = false;
-		}
+        EditingFile.Save(filePath);
 
-		public bool ShowSaveQuestion() {
-			if (!IsSaved) {
-				MessageBoxResult result = MessageBox.Show(
-					$"작업 중인 파일이 저장되지 않았습니다.{Environment.NewLine}" +
-					$"저장하시겠습니까?", "저장", MessageBoxButton.YesNoCancel);
-				switch (result) {
-					case MessageBoxResult.Yes:
-						return SaveFile();
-					case MessageBoxResult.No:
-						return true;
-					default:
-					case MessageBoxResult.Cancel:
-						return false;
-				}
-			} else {
-				return true;
-			}
-		}
+        MarkSaved();
+        return true;
+    }
 
-		private void RegisterFileEvents(MotionFile file) {
-			file.ItemCreated += MotionTab.EditingFile_ItemCreated;
-			file.ItemRemoved += MotionTab.EditingFile_ItemRemoved;
+    public bool CloseFile(bool checkSaved = true) {
+        if (!OnEditing) {
+            return true;
+        }
 
-			MotionTab.EditingFile_ItemCreated(EditingFile.rootFolder, null);
-		}
-		private void UnregisterFileEvents(MotionFile file) {
-			file.ItemCreated -= MotionTab.EditingFile_ItemCreated;
-			file.ItemRemoved -= MotionTab.EditingFile_ItemRemoved;
+        if (checkSaved && !ShowSaveQuestion()) {
+            return false;
+        }
 
-			MotionTab.EditingFile_ItemRemoved(EditingFile.rootFolder, null);
-		}
+        UnregisterFileEvents(EditingFile);
 
-		//Events
-		private void OnTick() {
-			//ApplyPreviewFPS();
-		}
+        MotionTab.ClearItems();
+        GraphEditorTab.DetachMotion();
+        EditingFile = null;
 
-		private string GetSaveFilename() {
-			if (EditingFile.IsFilePathAvailable) {
-				return EditingFile.filePath;
-			} else {
-				SaveFileDialog dialog = new SaveFileDialog();
-				dialog.Filter = IOInfo.Filter;
-				dialog.DefaultExt = IOInfo.Extension;
+        return true;
+    }
 
-				bool? result = dialog.ShowDialog();
-				if (result != null && result.Value) {
-					EditingFile.filePath = dialog.FileName;
-					return dialog.FileName;
-				} else {
-					return null;
-				}
-			}
-		}
-		private string GetOpenFilename() {
-			OpenFileDialog dialog = new OpenFileDialog();
-			dialog.DefaultExt = IOInfo.Extension;
-			dialog.Filter = IOInfo.Filter;
-			bool? result = dialog.ShowDialog();
+    public void MarkSaved() {
+        IsSaved = true;
+    }
 
-			if (result != null && result.Value == true) {
-				return dialog.FileName;
-			} else {
-				return null;
-			}
-		}
-	}
+    public void MarkUnsaved() {
+        IsSaved = false;
+    }
+
+    public bool ShowSaveQuestion() {
+        if (!IsSaved) {
+            MessageBoxResult result = MessageBox.Show($"작업 중인 파일이 저장되지 않았습니다.{Environment.NewLine}" + $"저장하시겠습니까?", "저장", MessageBoxButton.YesNoCancel);
+            switch (result) {
+                case MessageBoxResult.Yes:
+                    return SaveFile();
+                case MessageBoxResult.No:
+                    return true;
+                default:
+                case MessageBoxResult.Cancel:
+                    return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    private void RegisterFileEvents(MotionFile file) {
+        file.ItemCreated += MotionTab.EditingFile_ItemCreated;
+        file.ItemRemoved += MotionTab.EditingFile_ItemRemoved;
+
+        MotionTab.EditingFile_ItemCreated(EditingFile.rootFolder, null);
+    }
+
+    private void UnregisterFileEvents(MotionFile file) {
+        file.ItemCreated -= MotionTab.EditingFile_ItemCreated;
+        file.ItemRemoved -= MotionTab.EditingFile_ItemRemoved;
+
+        MotionTab.EditingFile_ItemRemoved(EditingFile.rootFolder, null);
+    }
+
+    //Events
+    private void OnTick() {
+        //ApplyPreviewFPS();
+    }
+
+    private string GetSaveFilename() {
+        if (EditingFile.IsFilePathAvailable) {
+            return EditingFile.filePath;
+        } else {
+            SaveFileDialog dialog = new();
+            dialog.Filter = IOInfo.Filter;
+            dialog.DefaultExt = IOInfo.Extension;
+
+            bool? result = dialog.ShowDialog();
+            if (result != null && result.Value) {
+                EditingFile.filePath = dialog.FileName;
+                return dialog.FileName;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private string GetOpenFilename() {
+        OpenFileDialog dialog = new();
+        dialog.DefaultExt = IOInfo.Extension;
+        dialog.Filter = IOInfo.Filter;
+        bool? result = dialog.ShowDialog();
+
+        if (result != null && result.Value == true) {
+            return dialog.FileName;
+        } else {
+            return null;
+        }
+    }
 }
